1e6d20e5f2b90faf32592a2137d9b31c573f6af3
1602366192 <33444395+1602366192@users.noreply.github.com>
2017-11-16 13:11:16 +0800

    Refactoring the code

--- Algorithm.go
+++ Algorithm.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+type Node struct {
+	data int
+	next *Node
+}
+
+func printList(head *Node) {
+	for (head != nil) {
+		fmt.Print(head.data)
+		fmt.Print(" ")
+		head = head.next
+	}
+	fmt.Println()
+}
+
 func main() {
-	fmt.Println("hello go")
+	var list1 Node
+	list1.data = 2
+
+	var item1 Node
+	item1.data = 5
+	list1.next = &item1
+
+	var item2 Node
+	item2.data = 8
+	item1.next = &item2
+	item2.next = nil
+	printList(&list1)
+
 }
